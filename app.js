ff99598fb1c724759a809e63c8d854d486bd406b
v-lai <lai.vivian@gmail.com>
2017-02-14 23:57:27 -0800

    add actual functionality

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var app = express();
 
+// ROOT ROUTE
 app.get("/", function(req, res){
-  res.send("hello world");
+  var ip = req.ip;
+  // var ip = req.connection.remoteAddress;
+
+  var language = req.headers['accept-language'].split(',')[0];
+  // var language = req.acceptsLanguages()[0];
+
+  var software = req.headers['user-agent'].split(')')[0].split('(')[1];
+
+  res.send(JSON.stringify({ ipaddress: ip, language: language, software: software }, null, '\t'));
+});
+
+// ANY OTHER ROUTE
+app.get("*", function(req, res){
+  res.send("Page not found.");
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
